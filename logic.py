d5709b251496de697071c4e358ae8f422f3e58f5
Cristina del Aguila <cristinadam@correo.ugr.es>
2024-11-25 10:41:40 +0100

    Fix PYTHONPATH for GitHub Actions

--- logic.py
+++ logic.py
@@ -1,16 +1,19 @@
 #Clase residente
 class Resident:
     def __init__(self, name, age, medication_schedule=None):
+        if age < 0:
+            raise ValueError("Age cannot be negative")
         self.name = name
         self.age = age
         self.medication_schedule = medication_schedule or []
 
     def add_medication(self, medication):
-        self.medication_schedule.append(medication)
+        if medication not in self.medication_schedule:
+            self.medication_schedule.append(medication)
 
 #Generar reportes de cuidado
 def generate_care_report(residents):
     return[
-        {"name": resident.name, "age": resident.age, "medication": resident.medication_schedule}
+        {"name": resident.name, "age": resident.age, "medications": resident.medication_schedule}
         for resident in residents
     ]
